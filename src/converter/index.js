d88eac60982306f5a714b25776ae96dce0c70ccc
Guillaume Chau <alphadelta.fg@gmail.com>
2019-06-23 20:23:56 +0200

    feat: wip heuristic grouping

--- src/converter/index.js
+++ src/converter/index.js
@@ -1,5 +1,7 @@
 import { parse, print, types, visit } from 'recast'
 import { camel, kebab } from 'case'
+import levenshtein from 'js-levenshtein'
+import stemmer from 'stemmer'
 
 const { namedTypes, builders } = types
 
@@ -243,25 +245,28 @@ export function convertScript(script) {
 
   // Lifecycle hooks
   const processHooks = (hookList, importList) => {
-  for (const option of options) {
+    for (const option of options) {
       if (hookList.includes(option.key.name)) {
-      const hookName = camel(`on_${option.key.name}`)
+        const hookName = camel(`on_${option.key.name}`)
         importList.push(hookName)
-      setupFn.body.body.push(builders.expressionStatement(
-        builders.callExpression(
-          builders.identifier(hookName),
-          [builders.arrowFunctionExpression(option.value.params, option.value.body)],
-        )
-      ))
-      removeOption(option)
+        setupFn.body.body.push(builders.expressionStatement(
+          builders.callExpression(
+            builders.identifier(hookName),
+            [builders.arrowFunctionExpression(option.value.params, option.value.body)],
+          )
+        ))
+        removeOption(option)
+      }
     }
-  }
   }
   processHooks(LIFECYCLE_HOOKS, newImports.vue)
   processHooks(ROUTER_HOOKS, newImports.vueRouter)
 
   // Remove `this`
   transformThis(setupFn.body.body, setupVariables, valueWrappers)
+
+  // Group statements heuristically
+  setupFn.body.body = groupStatements(setupFn.body.body, setupVariables)
 
   setupFn.body.body.push(setupReturn)
   componentDefinition.declaration.properties.push(
@@ -280,7 +285,7 @@ export function convertScript(script) {
       const specifiers = newImports[key].map(i => builders.importSpecifier(builders.identifier(i)))
       const importDeclaration = builders.importDeclaration(specifiers, builders.stringLiteral(pkg))
       importStatements.push(importDeclaration)
-  }
+    }
   }
   if (importStatements.length) {
     ast.program.body.splice(0, 0, ...importStatements, `\n`)
@@ -311,3 +316,204 @@ function transformThis (node, setupVariables, valueWrappers) {
     },
   })
 }
+
+/**
+ * @typedef Word
+ * @prop {string} value
+ * @prop {number} score
+ */
+
+/**
+ * @param {import('recast').types.ASTNode[]} nodes
+ * @param {string[]} setupVariables
+ */
+function groupStatements (nodes, setupVariables) {
+  // const stemmedVariableStats = getSetupVariableStats(nodes, setupVariables, true)
+  // console.log(stemmedVariableStats)
+
+  /** @type {{ nodes: Set, score: number }[]} */
+  let groups = []
+
+  // Classify nodes
+  const wordedNodes = []
+  const unwordedNodes = []
+  for (const node of nodes) {
+    if (getStatementWords(node, setupVariables).length) {
+      wordedNodes.push(node)
+    } else {
+      unwordedNodes.push(node)
+    }
+  }
+
+  // Group nodes together
+  for (const nodeA of wordedNodes) {
+    for (const nodeB of wordedNodes) {
+      if (nodeA !== nodeB) {
+        const score = getStatementGroupScore(nodeA, nodeB, setupVariables)
+        if (score > 0) {
+          let group = groups.find(
+            g => g.score === score && (g.nodes.has(nodeA) || g.nodes.has(nodeB))
+          )
+          if (!group) {
+            group = {
+              nodes: new Set(),
+              score,
+            }
+            groups.push(group)
+          }
+
+          [nodeA, nodeB].forEach(node => {
+            group.nodes.add(node)
+          })
+        }
+      }
+    }
+  }
+
+  // Remove duplicates
+  for (const node of wordedNodes) {
+    let bestGroup
+    const relevantGroups = []
+    for (const group of groups) {
+      if (group.nodes.has(node)) {
+        if (!bestGroup || group.score > bestGroup.score) {
+          bestGroup = group
+        }
+        relevantGroups.push(group)
+      }
+    }
+    // Remove the duplicated node in the not best groups
+    for (const group of relevantGroups) {
+      if (group !== bestGroup) {
+        group.nodes.delete(node)
+      }
+    }
+  }
+
+  // Sort groups
+  groups = groups.filter(g => g.nodes.size).sort((a, b) => b.score - a.score)
+
+  for (const group of groups) {
+    console.log('group score:', group.score, 'statements:', Array.from(group.nodes).map(node => print(node).code))
+  }
+
+  const result = []
+
+  for (const group of groups) {
+    result.push(...group.nodes)
+  }
+
+  result.push(...unwordedNodes)
+
+  return result
+}
+
+/**
+ * Returns a measure of how close two statements should be
+ * @param {import('recast').types.ASTNode} nodeA
+ * @param {import('recast').types.ASTNode} nodeB
+ * @param {{ [key: string]: number }} variableStats
+ */
+function getStatementGroupScore (nodeA, nodeB, setupVariables) {
+  const wordsA = getStatementWords(nodeA, setupVariables)
+  const wordsB = getStatementWords(nodeB, setupVariables)
+  let score = 0
+  for (const wordA of wordsA) {
+    for (const wordB of wordsB) {
+      const distance = levenshtein(wordA.value, wordB.value)
+      if (distance <= 1) {
+        score += (wordA.score + wordB.score) / 2
+      }
+    }
+  }
+  return score
+}
+
+const wordNodeCache = new Map()
+const wordCache = new Map()
+
+/**
+ * @param {import('recast').types.ASTNode} node
+ * @param {string[]} setupVariables
+ * @returns {Word[]}
+ */
+function getStatementWords (node, setupVariables) {
+  if (!wordNodeCache.has(node)) {
+    /** @type {Word[]} */
+    let words = []
+    // Variable
+    if (namedTypes.VariableDeclaration.check(node)) {
+      words.push({ value: node.declarations[0].id.name, score: 1 })
+    } else {
+      // Contained identifiers
+      visit(node, {
+        visitIdentifier (path) {
+          let identifier = path.value.name
+          if (setupVariables.includes(identifier) && !words.includes(identifier)) {
+            console.log(path)
+            words.push({ value: identifier, score: 1 })
+          }
+          this.traverse(path)
+        },
+      })
+    }
+
+    // Processing
+    const allWords = words.map(n => n.value).join('|')
+    if (wordCache.has(allWords)) {
+      words = wordCache.get(allWords)
+    } else {
+      words = processWords(words)
+      wordCache.set(allWords, words)
+    }
+    wordNodeCache.set(node, words)
+    return words
+  } else {
+    return wordNodeCache.get(node)
+  }
+}
+
+/**
+ * Separate & stem words
+ * @param {Word[]} words
+ * @returns {Word[]}
+ */
+function processWords (words) {
+  return words.reduce((list, word) => {
+    list.push(...kebab(word.value).split('-').map(value => ({
+      value: stemmer(value),
+      score: word.score,
+    })))
+    return list
+  }, [])
+}
+
+// function getSetupVariableStats (ast, setupVariables, stemming = false) {
+//   const stats = {}
+//   visit(ast, {
+//     visitIdentifier(path) {
+//       let identifier = path.value.name
+//       if (setupVariables.includes(identifier)) {
+//         addVariableStats(stats, identifier, stemming)
+//       }
+//       this.traverse(path)
+//     },
+//   })
+//   return stats
+// }
+
+// function addVariableStats (stats, identifier, stemming = false, update = value => value + 1) {
+//   let ids = [identifier]
+
+//   if (stemming) {
+//     ids = processNames(ids)
+//   }
+  
+//   for (const id of ids) {
+//     if (!stats[id]) {
+//       stats[id] = update(0)
+//     } else {
+//       stats[id] += update(stats[id])
+//     }
+//   }
+// }
